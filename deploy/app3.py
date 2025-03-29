11ed39e9d1a2a08f75885facc04d39a1e287d78f
julianxitoso <julianxitoso@gmail.com>
2025-03-29 11:47:35 -0500

    cambios

--- deploy/app3.py
+++ deploy/app3.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import routerPredictions
 
@@ -6,6 +7,13 @@ app = FastAPI()
 
 app.include_router(routerPredictions.router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes cambiar "*" por ["http://127.0.0.1:5500&quot;] si quieres más seguridad
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
+)
 
 if __name__=="__main__":
     app.run()
