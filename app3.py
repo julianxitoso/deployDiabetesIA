e4963d7d79a4aecadfbf2fe93d2a3e04102745e7
julianxitoso <julianxitoso@gmail.com>
2025-03-29 12:10:37 -0500

    cambios

--- app3.py
+++ app3.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import routerPredictions
 
 app = FastAPI()
 
 app.include_router(routerPredictions.router)
+
 
 
 if __name__=="__main__":
